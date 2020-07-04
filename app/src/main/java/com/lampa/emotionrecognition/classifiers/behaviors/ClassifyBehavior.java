ad68ec77de5f2a8a9bb45eee1c149107aaf4a6dd
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-05 00:00:08 +0700

    Remove some excess comments

--- app/src/main/java/com/lampa/emotionrecognition/classifiers/behaviors/ClassifyBehavior.java
+++ app/src/main/java/com/lampa/emotionrecognition/classifiers/behaviors/ClassifyBehavior.java
@@ -1,7 +1,5 @@
 package com.lampa.emotionrecognition.classifiers.behaviors;
 
-// Интерфейс для некоторого классификатора
 public interface ClassifyBehavior {
-    // Метод классификации
     float[][] classify(float[] input);
 }
